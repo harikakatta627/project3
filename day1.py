30938fc448604131e0782a01763faaed6a4041b5
harikakatta627 <167598132+harikakatta627@users.noreply.github.com>
2025-10-28 09:17:52 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+file_path_alt = "C:/Users/Admin/Downloads/student_exam_scores (1).csv"
+
+try:
+    df = pd.read_csv(file_path_alt)
+    print("--- File Loaded Successfully (using forward slashes) ---")
+    print(df.head())
+
+except FileNotFoundError:
+    print(f"Error: File not found at path: {file_path_alt}")
+except Exception as e:
+    print(f"An error occurred: {e}")
